759a0268d29f94c01b4fde07a032a81fc66c784c
Matthew Chick <c.chick@unsw.edu.au>
2019-11-06 15:51:52 +1100

    modify django admin to support changing user model

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from core import models
+
+# https://docs.djangoproject.com/en/2.2/ref/contrib/admin/
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    # control which fields are displayed on the change list page of the admin
+    list_display = ['email', 'name']
+    # control the layout of admin "add" and "change" pages
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (_('Permissions'),{'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
+
+admin.site.register(models.User, UserAdmin)
+
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -32,3 +32,10 @@ class AdminSiteTests(TestCase):
         # name field is not available in default UserAdmin class
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works # /admin/core/user/1"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
